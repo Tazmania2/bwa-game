da0c7c6657f9c4048bac0a564de88a302cf50b11
Tazmania2 <TCRabelo@gmail.com>
2025-12-03 15:41:41 -0300

    fixing mapping

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,26 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 // import {ToastrModule} from 'ngx-toastr';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './layout/app/app.component';
-import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
-import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
-import {MainModule} from './layout/main/main.module';
-import {CustomTranslateLoader} from './providers/custom-translate-loader';
-import {Chart, registerables} from 'chart.js';
-import {LottieModule} from 'ngx-lottie';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import {SemPermissaoComponent} from './layout/sem-permissao/sem-permissao.component';
-import {SharedModule} from './shared.module';
-import {BreveComponent} from './layout/breve/breve.component';
-import {C4uAnimacaoCidModule} from './components/c4u-animacao-cid/c4u-animacao-cid.module';
-import {MatSnackBarModule} from '@angular/material/snack-bar';
-import {C4uSpinnerModule} from "@components/c4u-spinner/c4u-spinner.module";
-import {AuthInterceptor} from './providers/auth.interceptor';
-import {NotificationService} from './services/notification.service';
-import {OverlayModule} from '@angular/cdk/overlay';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './layout/app/app.component';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { MainModule } from './layout/main/main.module';
+import { CustomTranslateLoader } from './providers/custom-translate-loader';
+import { Chart, registerables } from 'chart.js';
+import { LottieModule } from 'ngx-lottie';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SemPermissaoComponent } from './layout/sem-permissao/sem-permissao.component';
+import { SharedModule } from './shared.module';
+import { BreveComponent } from './layout/breve/breve.component';
+import { C4uAnimacaoCidModule } from './components/c4u-animacao-cid/c4u-animacao-cid.module';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { C4uSpinnerModule } from '@components/c4u-spinner/c4u-spinner.module';
+import { AuthInterceptor } from './providers/auth.interceptor';
+import { NotificationService } from './services/notification.service';
+import { OverlayModule } from '@angular/cdk/overlay';
 
 Chart.register(...registerables);
 
@@ -63,6 +63,10 @@ Chart.register(...registerables);
       provide: HTTP_INTERCEPTORS,
       useClass: AuthInterceptor,
       multi: true
+    },
+    {
+      provide: LOCALE_ID,
+      useValue: 'pt-BR'
     },
     NotificationService
   ],
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,19 @@
 /// <reference types="@angular/localize" />
 
 import { enableProdMode } from '@angular/core';
-import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
 
-import {AppModule} from './app/app.module';
+import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+// Register pt-BR locale for number and date pipes
+registerLocaleData(localePt, 'pt-BR');
+
 if (environment.production) {
-    enableProdMode();
-  }
+  enableProdMode();
+}
 
 platformBrowserDynamic().bootstrapModule(AppModule)
-    .catch(err => console.error(err));
+  .catch(err => console.error(err));
